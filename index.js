3b8fe3f7fa33f7b7c49593e2778bb2792c628d82
shahan <shahan@gmail.com>
2023-05-25 17:04:45 +0500

    feat: post method returns json res. add: 404 status

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const asyncHandler = require('express-async-handler')
+const multer = require('multer')
+const upload = multer({
+  dest: 'uploads/'
+})
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +16,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const uploadFile_cb = async(req, res, next) => {
+  const { originalname, mimetype, size } = req.file
+  return res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+}
+app.post('/api/fileanalyse', upload.single('upfile'), asyncHandler(uploadFile_cb))
 
+const notFound_cb = async(req, res, next) => {
+  res.status(404),
+  res.type('txt').send('Not found')
+}
 
+app.use(asyncHandler(notFound_cb))
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
